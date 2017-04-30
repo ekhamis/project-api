b02ed47b3128d10b4631c92b2168fde42401ee33
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Pro.local>
2017-04-30 15:16:15 +0200

    git commit updates

---
+++ database/migrations/sqls/20170430125529-products-up.sql
@@ -0,0 +1,7 @@
+/* Replace with your SQL commands */
+CREATE TABLE products (
+  product_id SMALLINT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(50) NOT NULL,
+  product_value DECIMAL(5,2) NOT NULL,
+  PRIMARY KEY (product_id)
+);
